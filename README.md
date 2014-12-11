f02cb445140e1e086590eaef16f668a7f19e26dd
Jazer Abreu <sokartema@gmail.com>
2014-12-11 11:39:37 +0000

    Push inicial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Ejemplos de WebGL
+
+### Funcionalidad
+
+Esta app permite visualizar diferentes ejemplos de WebGL
+
+### Modo de uso
+
+Antes de iniciar el servidor ejecutar `bundle install`
+
+Para arrancar el servidor situese en el directorio y ejecute `rackup` o `rake init` luego abra un navegador y vaya a la dirección localhost:9292 en el caso de rackup y localhost:4567 en el caso de rake init.
+
+### Heroku
+
+[Pagina de heroku](http://raphaeljs.herokuapp.com)
+
+**Jazer Abreu -> alu0100595727**
+
+Sistemas y Tecnologias Web, ETSII, Universidad de la Laguna.
+
+![ULL](http://www.ull.es/Public/images/wull/logo.gif)
---
+++ webgl.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+
+array = ["yellow","orange","red","purple","blue","turquoise","green"]
+
+set :environment , :development
+
+get '/' do
+
+  @title = "Index"
+
+  @color = array[rand(array.size)]
+
+  haml :index
+
+end
+
+get '/ejemplos/:ejemplo' do
+
+  ejemplo = params[:ejemplo]
+
+  @color = array[rand(array.size)]
+
+  case ejemplo
+
+  when "ejemplo1"
+
+    @title = "Ejemplo1"
+
+    haml :ejemplo1
+
+  else
+
+    @title = "Error"
+
+    haml :error
+
+  end
+end
+
+get '/help' do
+
+  @title = "Help"
+
+  @color = array[rand(array.size)]
+
+
+  haml :help
+
+end
